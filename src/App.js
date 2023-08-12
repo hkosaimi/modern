e82812e75fc8590971a0da24e0ea9520e7f4c3d3
Hussain <hkosaimi@gmail.com>
2023-08-12 23:42:47 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import "./App.css";
 
-import Box1 from "./components/Box1.jsx";
-import Box2 from "./components/Box2.jsx";
-import Box3 from "./components/Box3.jsx";
-import Box4 from "./components/Box4.jsx";
-import Box5 from "./components/Box5.jsx";
-import Box6 from "./components/Box6.jsx";
 function App() {
   return (
     <>
